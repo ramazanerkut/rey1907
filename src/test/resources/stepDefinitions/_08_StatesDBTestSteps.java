270d272a7df28c8c3158c5493739f3695f4ea00c
rey1907 <ramazan.yldz84@gmail.com>
2024-03-20 20:33:09 -0400

    Pomxml updated target

---
+++ src/test/resources/stepDefinitions/_08_StatesDBTestSteps.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _08_StatesDBTestSteps {
+    @And("Navigate to States Page")
+    public void navigateToStatesPage() {
+    }
+
+    @When("Send {string} as query")
+    public void sendAsQuery(String arg0) {
+    }
+
+    @Then("Check if the results from UI are in the database")
+    public void checkIfTheResultsFromUIAreInTheDatabase() {
+    }
+}
